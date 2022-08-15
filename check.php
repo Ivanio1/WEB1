3c72b9747e6819eebdd78a34e21aae872a452f97
Sobolev Ivan <sobolev1van@yandex.ru>
2022-08-15 13:12:03 +0500

    2
    Лабораторная №1 по ПИПу

--- check.php
+++ check.php
@@ -1,38 +1,73 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>response</title>
-    <meta charset="utf-8">
-</head>
-<body>
-
 <?php
 
-$start = microtime(true);
-
-date_default_timezone_set("UTC");
-if (array_key_exists("offset", $_GET)) {
-    $OFFSET = $_GET["offset"];
-} else $OFFSET = 0;
-//$time = time() + 3 * 3600;
-echo "<p class='script'>Текущее время: " . date("d.m.Y, G:i:s", $start - $OFFSET / 60 * 3600) . "</p>";
-
-if (!isset($_GET['x']) || !isset($_GET['y']) || !isset($_GET['r'])) die ("Вы выбрали не все переменные");
-
-$x = $_GET['x'];
-$y = $_GET['y'];
-$r = $_GET['r'];
-
-if ($y <= 0 && $x >= 0 && $y ** 2 + $x ** 2 <= $r ** 2 || $y >= 0 && $y <= $r && $x <= 0 && $x >= -$r  || $y >= 0 && $x >= 0 && $y + $x <= $r) {
-    echo '<p> Точка (' . $x . '; ' . $y . ') при параметре R = ' . $r . ' попала в закрашенную область! </p>';
-} else {
-    echo '<p class = "script"> Точка (' . $x . '; ' . $y . ') при параметре R = ' . $r . ' не попала в закрашенную область! </p>';
+// Validate functions
+function validateX($xVal) {
+    return isset($xVal);
 }
 
-$t = round((microtime(true) - $start), 7);
+function validateY($yVal) {
+    $Y_MIN = -3;
+    $Y_MAX = 5;
 
-echo '<p class="script">Время работы скрипта: ' . $t . " сек<br></p>";
+    if (!isset($yVal))
+        return false;
 
-?>
-</body>
-</html>
+    $numY = str_replace(',', '.', $yVal);
+    return is_numeric($numY) && $numY >= $Y_MIN && $numY <= $Y_MAX;
+}
+
+function validateR($rVal) {
+    return isset($rVal);
+}
+
+function validateForm($xVal, $yVal, $rVal) {
+    return validateX($xVal) && validateY($yVal) && validateR($rVal);
+}
+
+// Проверка фигур
+function checkTriangle($xVal, $yVal, $rVal) {
+    return $xVal >= 0 && $yVal >= 0 &&
+        $xVal + $yVal <=  $rVal;
+}
+
+function checkRectangle($xVal, $yVal, $rVal) {
+    return $xVal <= 0 && $yVal >= 0 &&
+        $xVal >= -$rVal && $yVal <= $rVal;
+}
+
+function checkCircle($xVal, $yVal, $rVal) {
+    return $xVal >=0 && $yVal <= 0 &&
+        sqrt($xVal*$xVal + $yVal*$yVal) <= $rVal;
+}
+
+function checkHit($xVal, $yVal, $rVal) {
+    return checkTriangle($xVal, $yVal, $rVal) || checkRectangle($xVal, $yVal, $rVal) ||
+        checkCircle($xVal, $yVal, $rVal);
+}
+
+// Основная программа
+$xVal = $_GET['xval'];
+$yVal = $_GET['yval'];
+$rVal = $_GET['rval'];
+date_default_timezone_set("UTC");
+$timezoneOffset = $_GET['timezone'];
+
+$isValid = validateForm($xVal, $yVal, $rVal);
+$converted_isValid = $isValid ? 'true' : 'false';
+$isHit = $isValid ? checkHit($xVal, $yVal, $rVal) : 'GG!';
+$converted_isHit = $isHit ? 'true' : 'false';
+
+$currentTime = date('H:i:s', time()-$timezoneOffset*60);
+$executionTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 7);
+
+$jsonData = '{' .
+    "\"validate\":$converted_isValid," .
+    "\"xval\":\"$xVal\"," .
+    "\"yval\":\"$yVal\"," .
+    "\"rval\":\"$rVal\"," .
+    "\"curtime\":\"$currentTime\"," .
+    "\"exectime\":\"$executionTime\"," .
+    "\"hitres\":$converted_isHit" .
+    "}";
+
+echo $jsonData;
---
+++ main.js
@@ -0,0 +1,54 @@
+function validate() {
+    let x = document.forms["input-form"]["xval"].value;
+    let y = document.forms["input-form"]["yval"].value;
+    let r = document.forms["input-form"]["rval"].value;
+
+    if (isNaN(x) || x === "") {
+        document.getElementById("trouble").innerHTML = "Выберите корректное значение Х";
+        return false;
+    }
+
+    if (y === "" || isNaN(y) || y>5||y<-3) {
+        document.getElementById("trouble").innerHTML = "Выберите корректное значение Y";
+        return false;
+    }if (y !== "" || !isNaN(y) || y<=5||y>=-3) {
+        document.getElementById("trouble").innerHTML = "";
+        return true;
+    }
+
+    if (isNaN(r) || r === "") {
+        document.getElementById("trouble").innerHTML = "Выберите корректное значение R";
+        return false;
+    }
+}
+
+function OnClick2(value, id) {
+    document.getElementById("MyEdit2").value = value;
+}
+$(function () {
+    $('#input-form').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: 'check.php',
+            method: 'GET',
+            data: $(this).serialize() + '&timezone=' + new Date().getTimezoneOffset(),
+            dataType: "json",
+            beforeSend: function () {
+                $('.button').attr('disabled', 'disabled');
+            },
+            success: function (data) {
+                $('.button').attr('disabled', false);
+                if (data.validate) {
+                    newRow = '<tr>';
+                    newRow += '<td>' + data.xval + '</td>';
+                    newRow += '<td>' + data.yval + '</td>';
+                    newRow += '<td>' + data.rval + '</td>';
+                    newRow += '<td>' + data.curtime + '</td>';
+                    newRow += '<td>' + data.exectime + '</td>';
+                    newRow += '<td>' + data.hitres + '</td>';
+                    $('#result-table').append(newRow);
+                }
+            }
+        });
+    });
+});
